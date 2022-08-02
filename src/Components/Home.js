106f600e0f1cfca5479842e43d3188def6f65278
Katherine Jean-Charles <katherinejc103@gmail.com>
2022-08-02 11:29:12 -0400

    edited homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,8 +5,21 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <h1>hello</h1>
-    
+        <container>
+
+        <h1>Katherine's Movies</h1>
+        <h1 align="center">My Own Personal Movie Review Blog</h1>
+        </container>
+       <container>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWIXqd7pzEOJn1XRK1j7ofDrQRXEURe2QDCaFjGvjWgo35hYU-Uyq6G2Jd-Ttp5VysDPE&usqp=CAU" class="center" width="400" height="500"/>
+        </container> 
+       <h2>Movie Review Key</h2>
+       <p>★★★★★ - LOVED IT!</p>
+       <p>★★★★☆ - Pretty Awesome but could've Been Better</p>
+       <p>★★★☆☆ - Liked it but would'nt re-watch</p>
+       <p>★★☆☆☆ - Just okay, would'nt recommend</p>
+       <p>★☆☆☆☆ - Was not a fan, could'nt keep me interested</p>
+       <p>☆☆☆☆☆ - WASTE OF TIME!</p>
     </div>
   )
 }
