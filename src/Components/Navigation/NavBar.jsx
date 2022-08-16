482bd9508f6d4dcce27cf9f72f522bbf6424ac38
Katherine Jean-Charles <katherinejc103@gmail.com>
2022-08-15 20:15:25 -0400

    removed link

--- src/Components/Navigation/NavBar.jsx
+++ src/Components/Navigation/NavBar.jsx
@@ -5,6 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -28,10 +29,10 @@ const NavBar = () => {
         <Container>
             <Toolbar>
             <Typography variant="h6" className={classes.title}>
-                Katherine's Movies
+                <Button color="inherit" component={ Link } to="/">Katherine's Movies</Button>
             </Typography>
-            <Button color="inherit">Movies</Button>
-            <Button color="inherit">Add New Movie</Button>
+            <Button color="inherit" component={ Link } to="/movies">Movies</Button>
+            <Button color="inherit" component={ Link } to="/movies/new">Add New Movie</Button>
             </Toolbar>
         </Container>
       </AppBar>
